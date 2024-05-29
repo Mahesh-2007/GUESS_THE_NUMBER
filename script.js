866eeed98823d0782a09d9dcd4a40b2d0b51e07c
Mahesh-2007 <mm2472229@gmail.com>
2024-05-29 14:00:08 +0530

    major changes

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ function ANSWER(){
 var c = a.value
 
 if(c==ans){
+    pop2()
     console.log("Right")
     b.textContent=("YOU ARE Right")
-    alert("YOU WON!(refresh for New Game )")
-   
+    
+    
 
 }else{
     console.log("Wrong")
@@ -20,7 +21,23 @@ if(c==ans){
     a.value="";
 }
 if(score==0){
-    alert("YOU LOST (refresh for New Game )")
+   pop3()
 }        
 console.log(ans)
+}
+var popup2 = document.querySelector(".popup2")
+var popup3 = document.querySelector(".popup3")
+var overlay2 = document.querySelector(".overlay2")
+function pop2(){
+
+    popup2.style.display = "block"
+    overlay2.style.display = "block"
+
+}
+function pop3(){
+    popup3.style.display = "block"
+    overlay2.style.display = "block"
+}
+function re(event){
+    location.reload(event);
 }
